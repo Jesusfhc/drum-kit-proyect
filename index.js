242c84fa5be879889c969fb42cc301968558e461
Jesusfhc <99701250+Jesusfhc@users.noreply.github.com>
2024-12-01 19:58:47 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,70 @@
+let btns = document.getElementsByClassName("drum");
+
+function checkSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break
+       case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break
+        case  "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break
+        case  "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break
+        case  "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break
+        case  "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break
+        case  "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break 
+        case "OP":
+            var op = new Audio("sounds/one-piece.mp3");
+            op.play();
+            buttonAnimation("emergency")
+        default:
+            console.log("default");
+            break
+    }
+} 
+
+function buttonAnimation(key) {
+    var currentBtn = document.querySelector("."+key);
+
+    currentBtn.classList.add("pressed");
+
+    setTimeout(function() {
+        currentBtn.classList.remove("pressed");
+    }, 100)
+}
+
+for (i = 0; i <= btns.length - 1; i++) {
+    btns[i].addEventListener("click", function() {
+
+        var btnClick = this.innerHTML;
+
+        checkSound(btnClick);
+        buttonAnimation(btnClick);
+        
+    });
+}
+
+document.addEventListener("keydown", function(event) {
+    
+    var btnKey = event.key;
+    
+    checkSound(btnKey);
+    buttonAnimation(btnKey)
+})
